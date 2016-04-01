76a3b9d0c3dc13820b05412711e0925937b4fda7
Fabrice <fab.lahitte@laposte.net>
2016-04-01 10:41:29 +0200

    vassé.php
    page vide

--- WEB/view/pdf.carte.php
+++ WEB/view/pdf.carte.php
@@ -18,11 +18,11 @@ $req_classe = $db->query($sql_classe, 'COAC\Table\Carte');
 
     while($data_classe = ($req_classe) )
     {
-           if ($data_classe['id'] == 138) {
        $sql = 'SELECT `Etudiant`.`Nom`, `Etudiant`.`Prenom`, `Etudiant`.`id_Promo`, Date_format(`Etudiant`.`Date_Naissance`,"%d/%m/%Y") AS DateNaissance , `Etudiant`.`id`, `Promo`.`Entree`, `Promo`.`Sortie`, `Promo`.`Filiere` FROM `COAC`.`Etudiant` AS `Etudiant`, `COAC`.`Promo` AS `Promo` WHERE `Etudiant`.`id_Promo` = `Promo`.`id_Promo` AND `Etudiant`.`id`='.$data_classe['id'].'';
         $req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
-         if($data = mysql_fetch_assoc($req))
+          if($data = mysql_fetch_assoc($req))
          {
+           if ($data_classe['id'] == 138) {
              $pdf->AddPage('L', array (74,46));
              $pdf->Image('images/CarteModele.png', null, null, 74, 46, 'PNG');
              $pdf->Image('http://192.168.1.200/includes/image.php?id='.$data['id'].'', $x+47, $y+4.3, 23, 28, 'JPG');
@@ -33,7 +33,7 @@ $req_classe = $db->query($sql_classe, 'COAC\Table\Carte');
              $pdf->Image('images/llf_tampon_alpha.png', $x+35, $y+23, 21, 12, 'PNG'); 
          }
 	}
-       }
-
+        }
+                    
 $pdf->Output('Carte_'.$classe.'.pdf', 'I');
 ?>
